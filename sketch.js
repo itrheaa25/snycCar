0e6fa240d10465b117c15de1d1b1843497181d96
itrheaa25 <76394554+itrheaa25@users.noreply.github.com>
2021-07-02 22:17:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var database;
+var gameState = 0;
+var playerCount;
+var form, player, game;
+
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  game = new Game()
+  game.start()
+  game.getState()
+
+
+
+
+  ;
+}
+
+function draw(){
+  background("white");
+  
+   
+    drawSprites();
+  
+}
+
+
